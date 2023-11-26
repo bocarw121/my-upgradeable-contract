48602fe4f54abb228475eb31a5647be36199f6be
bocarw121 <bocaralhassanwane@gmail.com>
2023-11-26 10:46:56 -0500

    Add VendingMachineV1 and setup proxy

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+dotenv.config();
+import { envConfig } from "./config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: envConfig.GOERLI_RPC_URL,
+      accounts: [envConfig.META_MASK_PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: envConfig.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
